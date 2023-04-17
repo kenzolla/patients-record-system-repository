53985c4706aaf8ce6a5399f84136eab8edb72f45
kenzolla <112931162+kenzolla@users.noreply.github.com>
2023-04-17 08:24:42 +0300

    Create contacts.php

---
+++ contacts.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contacts</title>
+</head>
+<body>
+<ul>
+  <li><a href="home.php">home</a></li>
+    <li><a href="index.php">index</a></li>
+    <li><a href="login.php">login</a></li>
+    <li><a href="signup.php">signup</a></li>
+    <li><a href="contacts.php">contacts</a></li>
+   
+  </ul>
+  <style>
+    ul{
+        display: inline-block;
+        color: black;
+        background color: gray;
+        text display: inline-block;
+
+    }
+  </style>
+  <h1>ALPHA PHARMACY PATIENT RECORD</h1>
+<div class="contact-details">
+  <h2>Contact Details</h2>
+  <ul>
+    <li><strong>Address:</strong> 246- kenneth's street-magwi</li>
+    <li><strong>Phone:</strong> (+256786570873)/+211920200664</li>
+    <li><strong>Email:</strong> www.alphapharmacy@gmail.com</li>
+    <li><strong>Hours:</strong> sunday-Friday: 9am-5pm</li>
+  </ul>
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<h3>Link with us through our social media below!</h3>
+<div class="social-icons">
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-instagram"></i></a>
+  <a href="#"><i class="fa fa-email"></i></a>
+  <a href="#"><i class="fa fa-youtube"></i></a>
+  <a href=""></a>
+</div>
+
+
+<style>
+.contact-details {
+  margin: 20px 0;
+  padding: 10px;
+  border: 5px solid #ccc;
+  border-radius: 5px;
+}
+
+.contact-details h2 {
+  font-size: 30px;
+  margin-bottom: 20px;
+}
+
+.contact-details ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.contact-details li {
+  margin-bottom: 5px;
+}
+
+.contact-details li strong {
+  width: 100px;
+  display: inline-block;
+  font-weight: bold;
+}
+/* Set the font size and padding for the icons */
+.social-icons a i {
+  font-size: 24px;
+  padding: 10px;
+}
+
+/* Style the icons with the appropriate color */
+.social-icons a i.fa-facebook {
+  color: #3b5998;
+}
+
+.social-icons a i.fa-twitter {
+  color: #1da1f2;
+}
+
+.social-icons a i.fa-instagram {
+  color: #e1306c;
+}
+
+/* Add hover effects to the icons */
+.social-icons a:hover i {
+  transform: scale(1.2);
+}
+
+.social-icons a:hover i.fa-facebook {
+  color: #fff;
+  background-color: #3b5998;
+}
+
+.social-icons a:hover i.fa-twitter {
+  color: #fff;
+  background-color: #1da1f2;
+}
+
+.social-icons a:hover i.fa-instagram {
+  color: brown;
+  background-color: gray;
+}
+
+
+</style>
+
+</body>
+</html>
